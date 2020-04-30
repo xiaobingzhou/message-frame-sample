2575f0d99a18d0e4e8c346c4c791fe6806108fcd
xiaobingzhou <bell.zhouxiaobing@staginfo.com>
2020-04-30 16:17:59 +0800

    update README.md

--- src/main/java/com/github/xiaobingzhou/messageframe/config/BindParamConfig.java
+++ src/main/java/com/github/xiaobingzhou/messageframe/config/BindParamConfig.java
@@ -5,16 +5,19 @@ import com.github.xiaobingzhou.messageframe.request.HandlerRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 自定义参数绑定器
+ */
 @Configuration
 public class BindParamConfig {
 
     @Bean
-    public BindParam<String> myBindParam() {
+    public BindParam<String> customeBindParam() {
         // 自定义参数绑定器，绑定参数名为：custome 和参数类型为：String.class 的参数，参数设置的值为 this is my custome param
         return new BindParam<String>() {
             @Override
-            public boolean support(String parameterName, Class<String> parameterType) {
-                return "custome".equals(parameterName) && String.class.isAssignableFrom(parameterType);
+            public boolean support(String parameterName) {
+                return "custome".equals(parameterName);
             }
 
             @Override
